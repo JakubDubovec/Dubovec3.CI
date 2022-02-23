11cfbde7d2fb7c4bcecb3ec0619fb5b57a67b269
z19istc07 <z19istc07@z00p3-04.SPSKNM>
2022-02-23 11:44:57 +0100

    Update Connection.php

---
+++ php_scripts/Connection.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "projekt2022"
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+sql = "SELECT id, username, password, FROM Users";
+result = mysqli_query($conn, $sql);
+
+print_r( mysqli_num_rows($result) );
+
+if ( mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - Name; " . $row["username"]. " " . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+?>
